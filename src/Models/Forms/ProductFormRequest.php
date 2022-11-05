ee3cebf4c0cdfb25edb7f5bcae6594537e0a5d82
walkerchiu <chenjen.chiou@gmail.com>
2022-11-05 08:15:27 +0800

    Chore(Vendor): Rename "php-site" to "php-site-mall"

--- src/Models/Forms/ProductFormRequest.php
+++ src/Models/Forms/ProductFormRequest.php
@@ -123,11 +123,11 @@ class ProductFormRequest extends FormRequest
                 && isset($data['host_id'])
             ) {
                 if (
-                    config('wk-mall-merchandise.onoff.site')
-                    && !empty(config('wk-core.class.site.site'))
-                    && $data['host_type'] == config('wk-core.class.site.site')
+                    config('wk-mall-merchandise.onoff.site-mall')
+                    && !empty(config('wk-core.class.site-mall.site'))
+                    && $data['host_type'] == config('wk-core.class.site-mall.site')
                 ) {
-                    $result = DB::table(config('wk-core.table.site.sites'))
+                    $result = DB::table(config('wk-core.table.site-mall.sites'))
                                 ->where('id', $data['host_id'])
                                 ->exists();
                     if (!$result)
